c0ff601b62d14c36303f2d436d9089cedcfb6cad
Will Hughes <willhughes01@gmail.com>
2022-02-15 22:17:18 -0800

    Add basic wordle bot that creates a thread when it sees wordle solvers.

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{channel::{Message, ChannelType}, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.bot {
+            return;
+        }
+        let content = msg.content.trim();
+        if let Some(rest) = content.strip_prefix("Wordle ") {
+            let day = rest.split_terminator('\n').collect::<Vec<_>>()[0].split_whitespace().collect::<Vec<_>>()[0];
+            let thread_name = format!("Wordle Solvers {}", day);
+            let chan = msg.channel_id.to_channel(&ctx.http).await.unwrap();
+            let threads = chan.guild().unwrap().guild_id.get_active_threads(&ctx.http).await.unwrap();
+            let thread = match threads.threads.iter().find(|t| t.name == thread_name) {
+                Some(t) => t.clone(),
+                None => {
+                        msg.channel_id.create_private_thread(&ctx, |f| {
+                            f.name(thread_name);
+                            f.kind(ChannelType::PublicThread);
+                            f.rate_limit_per_user(0);
+                            f
+                        }).await.unwrap()
+                }
+            };
+            thread.say(&ctx, format!("Congrats {}, welcome to the secret club", msg.author.mention())).await.unwrap();
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("WORDLE_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
